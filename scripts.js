0fac0f97b0d4513734bc9e078eea7975a5e3d433
Z4nettx <134246431+Z4nettx@users.noreply.github.com>
2024-06-05 23:07:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,8 +19,8 @@ document.addEventListener('DOMContentLoaded', function () {
             balloon.textContent = messages[messageIndex];
             
             // Posicionamento aleatório
-            const x = Math.random() * (window.innerWidth - 200);
-            const y = Math.random() * (window.innerHeight - 200);
+            const x = Math.random() * (window.innerWidth - 350);
+            const y = Math.random() * (window.innerHeight - 350);
             balloon.style.left = `${x}px`;
             balloon.style.top = `${y}px`;
 
